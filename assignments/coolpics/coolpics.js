d778c5d9c77346cff5a99606a17e1307b3c1760d
emilygunn <emily.gunndrgn@gmail.com>
2025-02-02 09:30:42 -0700

    Coolpics part 2 finished

--- assignments/coolpics/coolpics.js
+++ assignments/coolpics/coolpics.js
@@ -28,22 +28,24 @@ function viewerTemplate(src, alt) {
         </div>`;
 }
 
-function closeViewer(element) {
-    element.remove;
+function closeViewer() {
+    const viewerDiv = document.querySelector('.viewer')
+    viewerDiv.remove();
 }
 
 function viewHandler(event) {
     // create a variable to hold the element that was clicked on from event.target
     const clickedElement = event.target;
 	// get the src attribute from that element and 'split' it on the "-"
-    clickedElement.getAttribute('src').split('-')
+    let imgLink = clickedElement.getAttribute('src');
+    let imgParts = imgLink.split('-');
 	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
-    clickedElement.push('-full.jpeg')
+    imgParts[0] + '-full.jpeg';
 	// insert the viewerTemplate into the top of the body element
 	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
-    clickedElement.insertAdjacentHTML('afterbegin', viewerTemplate)
+    document.body.insertAdjacentHTML('afterbegin', viewerTemplate);
 	// add a listener to the close button (X) that calls a function called closeViewer when clicked
-    const closeButton = document.getElementsByClassName('close-viewer');
+    const closeButton = document.querySelector('.close-viewer');
     closeButton.addEventListener('click', closeViewer);
 }
 
