035875301bd24dc66b2e4c07583321e10ff343d6
emilygunn <emily.gunndrgn@gmail.com>
2025-02-14 21:13:11 -0700

    Arrays

--- assignments/arrays/arrays.js
+++ assignments/arrays/arrays.js
@@ -4,7 +4,7 @@ function listTemplate(step) {
     return `<li>${step}</li>`; //the html string made from step
 }
 const stepsHtml = steps.map(listTemplate); // use map to convert the list from strings to HTML
-//document.querySelector("#myList").innerHTML = stepsHtml.join("");// set the innerHTML
+document.querySelector("#myList").innerHTML = stepsHtml.join("");// set the innerHTML
 
 //Activity 2 - Map
 const grades = ['A', 'B', 'A'];
@@ -30,4 +30,23 @@ const gpa_cal = gpaPoints.reduce((total, item) => {
 }, 0);
 const gpa = gpa_cal / gpaPoints.length;
 
-console.log("GPA Score: " + gpa)
+console.log("GPA Score: " + gpa);
+
+//activity 4 - Filter
+
+const fruits = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+const fruitsFiltered = fruits.filter(function(fruit) {
+    return fruit.length < 6;
+})
+
+console.log(fruitsFiltered);
+
+//activity 5 - indexOf
+
+const numbers = [12, 34, 21, 54];
+const luckyNumber = 21;
+
+let luckyIndex = numbers.indexOf(luckyNumber);
+
+console.log('Lucky number in array is at index: ' + luckyIndex);
